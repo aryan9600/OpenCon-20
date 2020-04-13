7a1dd2cc13d983ea5f58facd726b0a114d8e2fdb
Sanskar Jaiswal <sanskarjaiswal@MacBook-Pro.local>
2020-04-13 17:00:43 +0530

    AuthScreen UI done.

--- lib/main.dart
+++ lib/main.dart
@@ -11,59 +11,30 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:provider/provider.dart';
 
 
-void main() async {
-  await DotEnv().load('.ENV');
-	 Map<PermissionGroup, PermissionStatus> permissions = await PermissionHandler().requestPermissions([PermissionGroup.microphone,]);
+void main(){
 	runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
-		return MultiProvider(
-			providers: [
-				ChangeNotifierProvider.value(value: Auth())
-			],
-		  child: Consumer<Auth>(
-			  builder: (ctx, auth, _) =>
-		    MaterialApp(
+		// return MultiProvider(
+		// 	providers: [
+		// 		ChangeNotifierProvider.value(value: Auth())
+		// 	],
+		//   child: Consumer<Auth>(
+		// 	  builder: (ctx, auth, _) =>
+		    return MaterialApp(
 		    	debugShowCheckedModeBanner: false,
 		    	title: 'OpenCon\'20',
-		    	theme: ThemeData(
-						brightness: Brightness.dark,
-						canvasColor: Color(0xff232526),
-						accentColor: Color(0xff00B7D0),
-						fontFamily: "Blinker",
-						primaryTextTheme: TextTheme(
-							headline: TextStyle(
-								color: Color(0xff00B7D0),
-								fontWeight: FontWeight.bold,
-							),
-							title: TextStyle(
-								color: Colors.white,
-								fontWeight: FontWeight.w400,
-							),
-							subtitle: TextStyle(
-								color: Color.fromRGBO(255, 255, 255, 0.5),
-							),
-						),
-						buttonTheme: ButtonThemeData(
-							buttonColor: Color(0xff00B7D0),
-							shape: RoundedRectangleBorder(
-								borderRadius: BorderRadius.circular(SizeConfig.blockSizeVertical),
-							),
-							padding: EdgeInsets.fromLTRB( 23,  1.5,  23,  1.8),
-							textTheme: ButtonTextTheme.normal
-						),
-		    	),
 		    	home: AuthScreen(),
 					routes: {
 						RegisterScreen.routeName: (ctx) => RegisterScreen(),
 						ProfileScreen.routeName: (ctx) => ProfileScreen()
 					},
-		    ),
-		  ),
-		);
+		    );
+		//   ),
+		// );
 	}
 }
 
--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -10,6 +10,9 @@ import 'package:open_con/utils/size_config.dart';
 import 'package:provider/provider.dart';
 import 'package:uni_links/uni_links.dart';
 
+import '../utils/size_config.dart';
+import '../utils/size_config.dart';
+
 
 class AuthScreen extends StatefulWidget {
 	@override
@@ -67,94 +70,98 @@ class _AuthScreenState extends State<AuthScreen> {
 		SizeConfig().init(context);
 		return Scaffold(
 			body: SafeArea(
-				child: Stack(
-					children: <Widget>[
-					// Background of the screen
-						Container(
-							color: Color(0xff232526),
-						),
-						SingleChildScrollView(
-							child: Center(
-								child: Column(
-									mainAxisSize: MainAxisSize.min,
-									crossAxisAlignment: CrossAxisAlignment.center,
-									children: <Widget>[
-										// OpenCon Logo
-										Container(
-											padding: EdgeInsets.only(top: SizeConfig.blockSizeVertical*70),
-											width: SizeConfig.blockSizeHorizontal*60,
-											decoration: BoxDecoration(
-												image: DecorationImage(
-													image: AssetImage('assets/opencon_logo.png')
-												)
-											),
-										),
-										Container(
-											width: SizeConfig.screenWidth/1.3,
-										  child: FlatButton(
-										  	color: Color(0xffF3F3F3),
-										  	textColor: Colors.black,
-										  	shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(SizeConfig.blockSizeVertical*1.5)),
-										  	child: Row(
-													mainAxisAlignment: MainAxisAlignment.spaceAround,
-									      	children: <Widget>[
-														Container(
-															child: Image.asset('assets/google_logo.png'),
-															width: SizeConfig.blockSizeHorizontal*13,
-														),
-														Text('Continue with Google', style: TextStyle(
-															fontFamily: 'Blinker',
-															color: Colors.black,
-															fontSize: SizeConfig.blockSizeVertical*3
-														),)
-									      	],
-												),
-										  	onPressed: () {
-										  		print('something');
-										  		try{
-										  			 Provider.of<Auth>(context, listen: false).signInWithGoogle().then((response) {
-															 print(response.uid);
-															Navigator.of(context).popAndPushNamed(ProfileScreen.routeName);
-													  });
-												  } catch(e){
-										  			throw e;
-												  }
-											  },
-										  ),
-										),
-										Container(
-											padding: EdgeInsets.only(top: SizeConfig.blockSizeVertical*5),
-											width: SizeConfig.screenWidth/1.3,
-										  child: FlatButton(
-										  	color: Color(0xffF3F3F3),
-										  	textColor: Colors.black,
-										  	shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(SizeConfig.blockSizeVertical*1.5)),
-										  	child: Row(
-													mainAxisAlignment: MainAxisAlignment.spaceAround,
-									      	children: <Widget>[
-														Container(
-															padding: EdgeInsets.fromLTRB(0, SizeConfig.blockSizeVertical*1.7, 0, SizeConfig.blockSizeVertical*1.7),
-															child: Image.asset('assets/github.png', height: SizeConfig.blockSizeVertical*4,),
-															width: SizeConfig.blockSizeHorizontal*13,
-														),
-														Text('Continue with GitHub', style: TextStyle(
-															fontFamily: 'Blinker',
-															color: Colors.black,
-															fontSize: SizeConfig.blockSizeVertical*3
-														),)
-									      	],
-												),
-												onPressed: (){
-													Provider.of<Auth>(context, listen: false).onClickGitHubLoginButton();
-												},
-											)
-										)
-									]
-								),
-							),
-						)
-					],
-				),
+				child: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: SizeConfig.screenHeight/8),
+                Center(
+                  child: Text('Welcome to', style: TextStyle(
+                    fontFamily: 'Blinker',
+                    fontSize: SizeConfig.blockSizeVertical*6,
+                  ),),
+                ),
+                Center(
+                  child: Text('OpenCon', style: TextStyle(
+                    fontFamily: 'Blinker',
+                    fontSize: SizeConfig.blockSizeVertical*6,
+                  ),),
+                ),
+                // OpenCon Logo
+                Container(
+                  padding: EdgeInsets.only(top: SizeConfig.blockSizeVertical*40),
+                  width: SizeConfig.blockSizeHorizontal*70,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage('assets/frame.png')
+                    )
+                  ),
+                ),
+                Container(
+                  width: SizeConfig.screenWidth/1.3,
+                  child: FlatButton(
+                    color: Colors.black,
+                    textColor: Colors.white,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(SizeConfig.blockSizeVertical*1.5)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.all(SizeConfig.blockSizeHorizontal*2),
+                          child: Image.asset('assets/google_logo.png'),
+                          width: SizeConfig.blockSizeHorizontal*13,
+                        ),
+                        Text('Continue with Google', style: TextStyle(
+                          fontFamily: 'Blinker',
+                          fontSize: SizeConfig.blockSizeVertical*3
+                        ),)
+                      ],
+                    ),
+                    onPressed: () {
+                      print('something');
+                      try{
+                          Provider.of<Auth>(context, listen: false).signInWithGoogle().then((response) {
+                            print(response.uid);
+                          Navigator.of(context).popAndPushNamed(ProfileScreen.routeName);
+                        });
+                      } catch(e){
+                        throw e;
+                      }
+                    },
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: SizeConfig.blockSizeVertical*3),
+                  width: SizeConfig.screenWidth/1.3,
+                  child: FlatButton(
+                    color: Colors.black,
+                    textColor: Colors.white,
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(SizeConfig.blockSizeVertical*1.5)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: <Widget>[
+                        Container(
+                          padding: EdgeInsets.fromLTRB(0, SizeConfig.blockSizeVertical*1.7, 0, SizeConfig.blockSizeVertical*1.7),
+                          child: Image.asset('assets/github.png', height: SizeConfig.blockSizeVertical*4,),
+                          width: SizeConfig.blockSizeHorizontal*13,
+                        ),
+                        Text('Continue with GitHub', style: TextStyle(
+                          fontFamily: 'Blinker',
+                          fontSize: SizeConfig.blockSizeVertical*3
+                        ),)
+                      ],
+                    ),
+                    onPressed: (){
+                      Provider.of<Auth>(context, listen: false).onClickGitHubLoginButton();
+                    },
+                  )
+                )
+              ]
+            ),
+          ),
+        )
 			),
 		);
 	}
