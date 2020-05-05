eb046131fa20cf9083ad165738d7b71975f051c3
Sanskar Jaiswal <sanskarjaiswal@Sanskars-MacBook-Pro.local>
2020-05-05 17:24:43 +0530

    Fixed bottomsheet and timeline order

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dialogflow/dialogflow_v2.dart';
-import 'package:open_con/backend/auth.dart';
 import 'package:open_con/utils/size_config.dart';
 import 'package:open_con/widgets/chat_message.dart';
 import 'package:open_con/widgets/slide_transition.dart';
-import 'dart:math' as math;
+
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 
 class ChatScreen extends StatefulWidget {
@@ -18,6 +18,7 @@ class _ChatScreenState extends State<ChatScreen> with SingleTickerProviderStateM
   final TextEditingController _textController = new TextEditingController();
   final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
   String userName = '';
+  PanelController _pc = new PanelController();
 
   @override
   initState(){
@@ -55,20 +56,12 @@ class _ChatScreenState extends State<ChatScreen> with SingleTickerProviderStateM
                 ),
               ),
             ),
-            new Container(
-              margin: EdgeInsets.only(right: SizeConfig.blockSizeHorizontal*3),
-              decoration: BoxDecoration(
-                color: Color(0xff00B7D0),
-                shape: BoxShape.circle
-              ),
+            GestureDetector(
+              onTap: () => _handleSubmitted(_textController.text,),
               child: Container(
-                padding: EdgeInsets.fromLTRB(SizeConfig.blockSizeHorizontal/1.2,0,0,SizeConfig.blockSizeHorizontal*1.5),
-                child: Transform.rotate(
-                  angle: 150 * math.pi/ 90,
-                  child: new IconButton(
-                    icon: new Icon(Icons.send, color: Colors.white),
-                    onPressed: () => _handleSubmitted(_textController.text,)),
-                ),
+                height: 52,
+                padding: EdgeInsets.fromLTRB(SizeConfig.blockSizeHorizontal,0,SizeConfig.blockSizeHorizontal*3,0),
+                child: Image.asset("assets/send.png",)
               ),
             ),
           ],
@@ -113,90 +106,40 @@ class _ChatScreenState extends State<ChatScreen> with SingleTickerProviderStateM
     });
     Response(text);
   }
-  void _showSheet() {
-    showModalBottomSheet(
-      context: context,
-      isScrollControlled: true,
-      builder: (_) {
-        return DraggableScrollableSheet(
-            expand: false,
-            builder: (_, controller) {
-              return Container(
-                decoration: BoxDecoration(
-                  boxShadow:[
-                    BoxShadow(
-                      offset: Offset(20, 0),
-                      color: Colors.grey[700],
-                      blurRadius: 20,
-                      spreadRadius: 10
-                    ),
-                    BoxShadow(
-                      offset: Offset(-20, 0),
-                      color: Colors.grey[700],
-                      blurRadius: 20,
-                      spreadRadius: 10
-                    )
-                  ],
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(24),
-                    topRight: Radius.circular(24)
-                  )
-                ),
-                child: ListView.separated(
-                  separatorBuilder: (context, index){
-                    if(index==0){
-                      return Container(height: 0, width: 0);
-                    } else {
-                      return Container(
-                        child: Divider(
-                          indent: SizeConfig.blockSizeHorizontal*7,
-                          endIndent: SizeConfig.blockSizeHorizontal*7,
-                          color: Colors.grey,
-                        ),
-                      );
-                    }
-                  },
-                  controller: controller,
-                  itemCount: 25,
-                  itemBuilder: (_, i) {
-                    if(i==0){
-                      return Center(
-                        child: Container(
-                          margin: EdgeInsets.symmetric(vertical: SizeConfig.blockSizeVertical*2.5),
-                          height: SizeConfig.blockSizeHorizontal,
-                          width: SizeConfig.blockSizeVertical*7,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(SizeConfig.blockSizeVertical),
-                            shape: BoxShape.rectangle,
-                            color: Colors.black,
-                          ),
-                        ),
-                      );
-                    } else {
-                     return ListTile(
-                       contentPadding: EdgeInsets.symmetric(horizontal: SizeConfig.blockSizeHorizontal*8),
-                        title: Text('OpenCon app question pls', style: TextStyle(
-                          fontFamily: 'Blinker',
-                          fontSize: SizeConfig.blockSizeVertical*3,
-                          fontWeight: FontWeight.w400,
-                          color: Color(0xff00B7D0)
-                        ),),
-                        subtitle: Text('Some generic answer hopefully long enough to fill the space', style: TextStyle(
-                          fontFamily: 'Blinker',
-                          fontSize: SizeConfig.blockSizeVertical*2,
-                          fontWeight: FontWeight.w300,
-                          color: Colors.black54
-                        )),
-                      );
-                    }
-                    
-                  }
-                ),
-              );
-            },
-        );
-      },
+
+  Widget _scrollList(ScrollController sc){
+    return Padding(
+      padding: const EdgeInsets.only(top: 32.0),
+      child: ListView.separated(
+        separatorBuilder: (ctx, i){
+          return Container(
+            child: Divider(
+              indent: SizeConfig.blockSizeHorizontal*7,
+              endIndent: SizeConfig.blockSizeHorizontal*7,
+              color: Colors.grey,
+            ),
+          );
+        },
+        controller: sc,
+        itemCount: 25,
+        itemBuilder: (ctx, i){
+          return ListTile(
+            contentPadding: EdgeInsets.symmetric(horizontal: SizeConfig.blockSizeHorizontal*8),
+            title: Text('OpenCon app question pls', style: TextStyle(
+              fontFamily: 'Blinker',
+              fontSize: 18,
+              fontWeight: FontWeight.w400,
+              color: Color(0xff00B7D0)
+            ),),
+            subtitle: Text('Some generic answer hopefully long enough to fill the space', style: TextStyle(
+              fontFamily: 'Blinker',
+              fontSize: 15,
+              fontWeight: FontWeight.w300,
+              color: Color(0xff232528)
+            )),
+          );
+        },
+      ),
     );
   }
 
@@ -204,90 +147,121 @@ class _ChatScreenState extends State<ChatScreen> with SingleTickerProviderStateM
   Widget build(BuildContext context) {
     // final _user = Provider.of<Auth>(context, listen: false);
     SizeConfig().init(context);
-    return new Scaffold(
-      key: _scaffoldKey,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(SizeConfig.blockSizeVertical*15),
-        child: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-          centerTitle: true,
-          leading: GestureDetector(
-            onTap: (){
-              Navigator.pop(context, SlideRightRoute);
-            },
-            child: OverflowBox(
-              minWidth: 30,
-              maxWidth: 60,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: Row(
+    return SafeArea(
+      child: new Scaffold(
+        key: _scaffoldKey,
+        body: Stack(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                SizedBox(height: SizeConfig.blockSizeVertical*3,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: <Widget>[
-                    Image.asset("assets/back.png", height: 12, width: 12,),
-                    Text(' Back', style: TextStyle(
-                      fontFamily: 'Blinker',
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black
-                    ),)
-                  ],
-                ),
-              ),
-            )
-          ),
-          actions: <Widget>[
-            GestureDetector(
-              onTap: () {
-                _showSheet();
-              },
-              child: Padding(
-                padding: const EdgeInsets.only(right: 8.0),
-                child: Row(
-                  children: <Widget>[
-                    Text('FAQ ', style: TextStyle(
-                      fontFamily: 'Blinker',
-                      fontSize: 20,
-                      fontWeight: FontWeight.w600,
-                      color: Color(0xff00B7D0)
-                    ),),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 3.0),
-                      child: Image.asset("assets/faq.png", height: 16, width: 16,),
+                    GestureDetector(
+                      onTap: (){
+                        Navigator.pop(context, SlideRightRoute);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Row(
+                          children: <Widget>[
+                            Image.asset("assets/back.png", height: 12, width: 12,),
+                            Text(' Back', style: TextStyle(
+                              fontFamily: 'Blinker',
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black
+                            ),)
+                          ],
+                        ),
+                      ),
                     ),
+                    GestureDetector(
+                      onTap: () {
+                        print('lol');
+                        _pc.animatePanelToPosition(0.4);
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: Row(
+                          children: <Widget>[
+                            Text('FAQ ', style: TextStyle(
+                              fontFamily: 'Blinker',
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: Color(0xff00B7D0)
+                            ),),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 3.0),
+                              child: Image.asset("assets/faq.png", height: 16, width: 16,),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
                   ],
                 ),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 25),
+                  child: Center(
+                    child: Text('Contact Us',
+                      style: TextStyle(
+                        fontFamily: 'Blinker',
+                        fontSize: 28,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w600
+                      ),
+                    ),
+                  ),
+                ),
+                Flexible(
+                    child: ListView.builder(
+                      padding: EdgeInsets.all(8.0),
+                      reverse: true,
+                      itemBuilder: (_, int index) => _messages[index],
+                      itemCount: _messages.length,
+                    )),
+                Container(
+                  // decoration: new BoxDecoration(color: Theme.of(context).cardColor),
+                  child: _buildTextComposer(),
+                ),
+            ]),
+            SlidingUpPanel(
+              header: Center(
+                child: Container(
+                  margin: EdgeInsets.symmetric(vertical: SizeConfig.blockSizeVertical*2.5, horizontal: SizeConfig.screenWidth/2.35),
+                  height: 4,
+                  width: SizeConfig.screenWidth/7,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(2),
+                    shape: BoxShape.rectangle,
+                    color: Colors.black,
+                  ),
+                ),
               ),
+              controller: _pc,
+              minHeight: 0,
+              maxHeight: SizeConfig.screenHeight/1.05,
+              snapPoint: 0.4,
+              backdropEnabled: true,
+              backdropColor: Colors.black,
+              backdropOpacity: 0.5,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  blurRadius: 6
+                )
+              ],
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(24.0),
+                topRight: Radius.circular(24.0),
+              ),
+              panelBuilder: (ScrollController sc) => _scrollList(sc),
             )
           ],
-          flexibleSpace: Center(
-            child: Container(
-              padding: EdgeInsets.only(top: SizeConfig.blockSizeVertical*5),
-              child: Text('Contact Us',
-                style: TextStyle(
-                  fontFamily: 'Blinker',
-                  fontSize: 32,
-                  color: Colors.black,
-                  fontWeight: FontWeight.w600
-                ),
-              ),
-            ),
-          )
-        ),
+        )
       ),
-      body:  Column(
-        children: <Widget>[
-          Flexible(
-              child: ListView.builder(
-                padding: EdgeInsets.all(8.0),
-                reverse: true,
-                itemBuilder: (_, int index) => _messages[index],
-                itemCount: _messages.length,
-              )),
-           Container(
-            // decoration: new BoxDecoration(color: Theme.of(context).cardColor),
-            child: _buildTextComposer(),
-          ),
-      ]),
     );
   }
 }
